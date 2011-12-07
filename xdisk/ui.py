e38285daf43c9e14719a97b643e8fec7073c7073
Eugene <yeo.eugene.oey@gmail.com>
2011-12-07 20:38:48 +0800

    display disk enumeration in the UI

--- xdisk/ui.py
+++ xdisk/ui.py
@@ -22,14 +22,27 @@ class Model:
 
 #-------------------------------------------------------------------------------
 import Tkinter
+import tkFont
 
 class View(Tkinter.Tk):
     def __init__(self):
         Tkinter.Tk.__init__(self)
-        self.diskList = Tkinter.Listbox(self)
-        self.diskList.pack()
+        self.geometry("600x400")
+        monospace = tkFont.Font(family='Courier',size=9)
+
+        self.diskList = Tkinter.Listbox(self,selectmode=Tkinter.EXTENDED)
+        self.diskList.grid(row=0,column=0,sticky=Tkinter.NSEW)
         self.detectBtn = Tkinter.Button(self,text='Detect')
-        self.detectBtn.pack()
+        self.detectBtn.grid(row=1,column=0,sticky=Tkinter.NSEW)
+        self.diskInfo = Tkinter.Listbox(self,font=monospace)
+        self.diskInfo.grid(row=0,column=1,rowspan=2,sticky=Tkinter.NSEW)
+        self.wipeBtn = Tkinter.Button(self,text='Detect')
+        self.wipeBtn.grid(row=2,column=1,sticky=Tkinter.NSEW)
+
+        self.rowconfigure(0,weight=1)
+        self.rowconfigure(1,weight=1)
+        self.columnconfigure(0)
+        self.columnconfigure(1,weight=2)
 
 
 #-------------------------------------------------------------------------------
@@ -38,14 +51,34 @@ class Controller:
         self.model = Model()
         self.view = View()
         self.view.detectBtn.config(command=self.detectDisk)
+        self.view.diskList.bind("<<ListboxSelect>>", self.diskListSelected)
         self.view.mainloop()
-
 
     def detectDisk(self):
         self.model.enumDisk()
         self.view.diskList.delete(0,Tkinter.END)
-        for disk in self.model.diskList:
-            self.view.diskList.insert(Tkinter.END,disk)
+        self.view.diskList.insert(Tkinter.END,*self.model.diskList)
+
+    def diskListSelected(self,event):
+        selection = event.widget.curselection()
+        selectedList = [self.view.diskList.get(int(item)) for item in selection]
+        self.diskSelected = [self.model.diskList[deviceId] for deviceId in selectedList]
+        self.displayDiskInfo()
+
+    def displayDiskInfo(self):
+        def heading(text, padding="-",width=60):
+            return '{0:{1}^{2}}'.format(text,padding,width)
+
+        def body(key,value,width=60):
+            return '{0: <17} : {1: >40}'.format(k,v)
+
+        self.view.diskInfo.delete(0,Tkinter.END)
+        for disk in self.diskSelected:
+            self.view.diskInfo.insert(Tkinter.END,heading(disk))
+            for k,v in disk.__dict__.iteritems():
+                self.view.diskInfo.insert(Tkinter.END, body(k,v))
+            self.view.diskInfo.insert(Tkinter.END,"")
+
 
 
 #-------------------------------------------------------------------------------
